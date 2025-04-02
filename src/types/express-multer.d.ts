1631e14059ace69a1e5a01bcf07b5516774bae96
Ismael Ponce <ismaelponceon@gmail.com>
2025-04-02 16:54:29 +0200

    Multer error

--- src/types/express-multer.d.ts
+++ src/types/express-multer.d.ts
@@ -2,6 +2,7 @@ import { Multer } from 'multer';
 
 declare global {
   namespace Express {
+    export interface Multer extends Multer {}
     interface Request {
       files?: Express.Multer.File[];
     }
