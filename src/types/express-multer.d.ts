9e2a8dcc82c74ea7db892a18bc5ef4bc5c5e9a24
Ismael Ponce <ismaelponceon@gmail.com>
2025-04-02 17:25:15 +0200

    m

--- src/types/express-multer.d.ts
+++ src/types/express-multer.d.ts
@@ -2,8 +2,21 @@ import { Multer } from 'multer';
 
 declare global {
   namespace Express {
+    namespace Multer {
+      interface File {
+        fieldname: string;
+        originalname: string;
+        encoding: string;
+        mimetype: string;
+        size: number;
+        destination: string;
+        filename: string;
+        path: string;
+        buffer: Buffer;
+      }
+    }
     interface Request {
-      files?: any[];
+      files?: Express.Multer.File[];
     }
   }
 }
